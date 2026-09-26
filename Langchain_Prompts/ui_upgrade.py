9a8b7289e781ee9d9ad38d2cdedb4568eaaf5101
raw9k <raunakgupta914@gmail.com>
2026-09-26 21:03:00 +0530

    Refactor prompt handling: load template from JSON and streamline UI integration

--- Langchain_Prompts/ui_upgrade.py
+++ Langchain_Prompts/ui_upgrade.py
@@ -1,11 +1,12 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 import streamlit as st
 
 load_dotenv()
 model = ChatGroq(model="openai/gpt-oss-20b")
+template = load_prompt('Langchain_Prompts/template.json')
 
 
 st.header("Research Tool")
@@ -16,24 +17,7 @@ style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "T
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-# Template
 
-template = PromptTemplate(
-    template="""
-    Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}  
-Explanation Length: {length_input}  
-1. Mathematical Details:  
-   - Include relevant mathematical equations if present in the paper.  
-   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-2. Analogies:  
-   - Use relatable analogies to simplify complex ideas.  
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
-Ensure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=['paper_input', 'style_input','length_input'],
-    
-)
 
 prompts = template.invoke({
     "paper_input" : paper_input,
